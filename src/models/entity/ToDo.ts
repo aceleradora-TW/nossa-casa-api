488c48cef64e828f8bff8425555c318f3302905d
nicofercavv-dev <nicoleferreira281@gmail.com>
2023-07-07 11:28:44 -0300

    refactor: comentando o model ToDo

--- src/models/entity/ToDo.ts
+++ src/models/entity/ToDo.ts
@@ -1,11 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+// import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
-export class ToDo {
-  @PrimaryGeneratedColumn()
-  id: number;
+// @Entity()
+// export class ToDo {
+//   @PrimaryGeneratedColumn()
+//   id: number;
 
-  @Column({ name: 'name', type: 'varchar' })
-  name: String;
+//   @Column({ name: 'name', type: 'varchar' })
+//   name: String;
 
-}
+// }
